13666c1c37aa42308a5f09c8038f545005dc12b9
dedurasawa <113485097+dedurasawa@users.noreply.github.com>
2022-09-27 11:41:52 -0300

    creando repositorio de los archivos del encriptador

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+let textoNormal = document.getElementById("textarea");
+let encriptado = document.getElementById("texto_encriptado")
+let botonEnc = document.getElementById("btn_encriptar"); //botonEnc y botonDes hacen referencia a los botones de encriptado y desencriptado respectivamente//
+let botonDes = document.getElementById("btn_desencriptar");
+let copiar = document.getElementById("btn_copiar") //boton que copia el mensaje encriptado
+let imagen = document.getElementById("img_desencriptado"); //imagen que va a desaparecer cuando aparezca el mensaje encriptado
+let codigo = ["ai", "enter", "imes", "over", "ufat"]
+
+
+function encriptar() {
+	
+	let caracteres = Array.from(textoNormal);
+
+
+}
+
+
